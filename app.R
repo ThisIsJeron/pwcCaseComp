3d1b083db43022a4da66624f047250fc99763f34
Jeron Wong <ThisIsJeron@users.noreply.github.com>
2018-10-25 13:39:55 -0500

    change to smaller ex

--- app.R
+++ app.R
@@ -1,51 +1,58 @@
 library(shiny)
 library(plotly)
+library(ggplot2movies)
 
-data(diamonds, package = "ggplot2")
-nms <- names(diamonds)
+minx <- min(movies$rating)
+maxx <- max(movies$rating)
 
 ui <- fluidPage(
   
   headerPanel("Fleetwings Quality of Life Data Visualization"),
   sidebarPanel(
-    sliderInput('sampleSize', 'Sample Size', min = 1, max = nrow(diamonds),
-                value = 1000, step = 500, round = 0),
-    selectInput('x', 'X', choices = nms, selected = "carat"),
-    selectInput('y', 'Y', choices = nms, selected = "price"),
-    selectInput('color', 'Color', choices = nms, selected = "clarity"),
-    
-    selectInput('facet_row', 'Facet Row', c(None = '.', nms), selected = "clarity"),
-    selectInput('facet_col', 'Facet Column', c(None = '.', nms)),
-    sliderInput('plotHeight', 'Height of plot (in pixels)', 
-                min = 100, max = 2000, value = 1000)
+    sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 10)
   ),
   mainPanel(
-    plotlyOutput('trendPlot', height = "900px")
+    plotlyOutput('trendPlot'),
+    plotlyOutput('trendPlot2'),
+    plotlyOutput('trendPlot3')
   )
 )
 
 server <- function(input, output) {
   
-  #add reactive data information. Dataset = built in diamonds data
-  dataset <- reactive({
-    diamonds[sample(nrow(diamonds), input$sampleSize),]
-  })
-  
   output$trendPlot <- renderPlotly({
+    # size of the bins depend on the input 'bins'
+    size <- (maxx - minx) / input$bins
     
-    # build graph with ggplot syntax
-    p <- ggplot(dataset(), aes_string(x = input$x, y = input$y, color = input$color)) + 
-      geom_point()
-    
-    # if at least one facet column/row is specified, add it
-    # facets <- paste(input$facet_row, '~', input$facet_col)
-    # if (facets != '. ~ .') p <- p + facet_grid(facets)
-    
-    ggplotly(p) %>% 
-      layout(height = input$plotHeight, autosize=TRUE)
-    
+    # a simple histogram of movie ratings
+    p <- plot_ly(movies, x = movies$rating, autobinx = F, type = "histogram",
+                 xbins = list(start = minx, end = maxx, size = size))
+    # style the xaxis
+    layout(p, xaxis = list(title = "Ratings", range = c(minx, maxx), autorange = F,
+                           autotick = F, tick0 = minx, dtick = size))
   })
-  
+  output$trendPlot2 <- renderPlotly({
+    # size of the bins depend on the input 'bins'
+    size <- (maxx - minx) / input$bins
+    
+    # a simple histogram of movie ratings
+    p <- plot_ly(movies, x = movies$rating, autobinx = F, type = "histogram",
+                 xbins = list(start = minx, end = maxx, size = size))
+    # style the xaxis
+    layout(p, xaxis = list(title = "Ratings", range = c(minx, maxx), autorange = F,
+                           autotick = F, tick0 = minx, dtick = size))
+  })
+  output$trendPlot3 <- renderPlotly({
+    # size of the bins depend on the input 'bins'
+    size <- (maxx - minx) / input$bins
+    
+    # a simple histogram of movie ratings
+    p <- plot_ly(movies, x = movies$rating, autobinx = F, type = "histogram",
+                 xbins = list(start = minx, end = maxx, size = size))
+    # style the xaxis
+    layout(p, xaxis = list(title = "Ratings", range = c(minx, maxx), autorange = F,
+                           autotick = F, tick0 = minx, dtick = size))
+  })
 }
 
 shinyApp(ui, server)
